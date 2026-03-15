b6956242568acd224f7eb213214781cd9780bdda
Игорь <gariksyr@gmail.com>
2026-03-15 21:30:11 +0300

    add swagger descriptions

--- src/main/java/com/thesis/vesselservice/controller/VesselController.java
+++ src/main/java/com/thesis/vesselservice/controller/VesselController.java
@@ -3,27 +3,32 @@ package com.thesis.vesselservice.controller;
 import com.thesis.vesselservice.dto.VesselRequestDTO;
 import com.thesis.vesselservice.dto.VesselResponseDTO;
 import com.thesis.vesselservice.service.VesselService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
+@Tag(name = "Vessel management")
 @RestController
 @RequestMapping("/api/v1/vessels")
 @RequiredArgsConstructor
 public class VesselController {
     private final VesselService vesselService;
+    @Operation(description = "Add new vessel to db")
     @PostMapping()
     public ResponseEntity<VesselResponseDTO> createVessels(@RequestBody @Valid VesselRequestDTO dto){
         VesselResponseDTO vessel = vesselService.registerVessel(dto);
         return new ResponseEntity<>(vessel, HttpStatus.CREATED);
     }
+    @Operation(description = "Get vessel with required IMO")
     @GetMapping("/{imo}")
     public ResponseEntity<VesselResponseDTO> getVessel(@PathVariable String imo){
         return new ResponseEntity<>(vesselService.getVessel(imo), HttpStatus.OK);
     }
+    @Operation(description = "Get all vessels")
     @GetMapping()
     public ResponseEntity<Page<VesselResponseDTO>> index(@RequestParam(name = "page", defaultValue = "0") int page,
                                                          @RequestParam(name = "size", defaultValue = "10") int size,
@@ -39,10 +44,12 @@ public class VesselController {
         }
         return ResponseEntity.ok(vesselService.findAll(page, size));
     }
+    @Operation(description = "Update vessel data")
     @PatchMapping()
     public ResponseEntity<VesselResponseDTO> updateVessel(@RequestParam Long id, @RequestBody @Valid VesselRequestDTO dto){
         return new ResponseEntity<>(vesselService.updateVessel(id, dto), HttpStatus.OK);
     }
+    @Operation(description = "Delete vessel")
     @DeleteMapping()
     public HttpStatus deleteVessel(@RequestParam Long id){
         vesselService.deleteVessel(id);
--- src/main/java/com/thesis/vesselservice/dto/VesselRequestDTO.java
+++ src/main/java/com/thesis/vesselservice/dto/VesselRequestDTO.java
@@ -1,18 +1,23 @@
 package com.thesis.vesselservice.dto;
 
 import com.thesis.vesselservice.util.ValidImo;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 @Data
 public class VesselRequestDTO {
     @NotBlank
+    @Schema(description = "Unique 7-digit vessel code", example = "9311622", requiredMode = Schema.RequiredMode.REQUIRED)
     @ValidImo
     private String IMO;
     @NotBlank
+    @Schema(description = "Vessel's name", example = "Igor")
     private String name;
     @NotBlank
+    @Schema(description = "Type of vessel", example = "Tanker")
     private String type;
     @NotBlank
+    @Schema(description = "Vessel's country", example = "Russia")
     private String flag;
 }
